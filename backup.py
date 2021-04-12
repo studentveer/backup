8bba74db6ab792f978b5bb98b82df77b12c27c20
studentveer <73462172+studentveer@users.noreply.github.com>
2021-04-12 13:16:18 +0530

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,5 @@
+# moving mp4 file from source to destination
+path="E:/youtube/bandicam 2021-02-21 17-56-31-355.mp4"
+
+destination="E:/bandicam 2021-02-21 17-56-31-355.mp4"
+dest=shutil.move(path,destination)
